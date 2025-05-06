e1d348c29ab5d743c2bb1dd657b509a07ac49140
Timi-Holberton <timi.holberton@gmail.com>
2025-05-06 14:03:01 +0200

    3.1 print alphabet sans E ni Q

---
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+# Affiche l'alphabet en minuscules sauf 'q' et 'e'
+for char in range(ord('a'), ord('z') + 1):
+    # Parcourt les codes ASCII de 'a' (97) à 'z' (122) inclus
+
+    if char != ord('e') and char != ord('q'):
+        # Vérifie que la lettre n'est pas 'e' (101) ni 'q' (113)
+
+        print(chr(char), end="")
+        # Affiche le caractère correspondant sans saut de ligne
