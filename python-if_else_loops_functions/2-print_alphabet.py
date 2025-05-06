c59405c925457a02e9732438848a8333fe85405f
Timi-Holberton <timi.holberton@gmail.com>
2025-05-06 14:03:07 +0200

    commentaire

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
+
+# Parcourt les entiers de 97 ('a') à 122 ('z') inclus
 for i in range(97, 123):
+    # Affiche le caractère correspondant à i sans saut de ligne
     print("{}".format(chr(i)), end="")
