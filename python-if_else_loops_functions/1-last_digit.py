1804bcac010087afa2cc4af5e47edef949e824dc
Timi-Holberton <timi.holberton@gmail.com>
2025-05-06 10:47:40 +0200

    1.4 respect PythonStyles avec retour à la ligne car ligne trop longue

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -11,6 +11,10 @@ else:
 if last_digit_signed == 0:
     print(f"Last digit of {number} is {last_digit_signed} and is 0")
 elif last_digit_signed > 5:
-    print(f"Last digit of {number} is {last_digit_signed} and is greater than 5")
+    print(
+        f"Last digit of {number} is {last_digit_signed} "
+        "and is greater than 5")
 else:
-    print(f"Last digit of {number} is {last_digit_signed} and is less than 6 and not 0")
+    print(
+        f"Last digit of {number} is {last_digit_signed} "
+        "and is less than 6 and not 0")
