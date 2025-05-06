ae1cdecd9147d2bb4588ec0b1c665cd120f7d80c
Timi-Holberton <timi.holberton@gmail.com>
2025-05-06 10:37:10 +0200

    1.2 signe négatif

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -2,12 +2,15 @@
 import random
 number = random.randint(-10000, 10000)
 last_digit = abs(number) % 10
-if last_digit > 5:
-    print(f"Last digit of {number} is {last_digit} and is greater than 5")
-elif last_digit < 6 and last_digit != 0:
-    if number < 0:
-        print(f"Last digit of {number} is -{last_digit} and is less than 6 and not 0")
-    else:
-        print(f"Last digit of {number} is {last_digit} and is less than 6 and not 0")
+if number < 0:
+    last_digit_signed = -last_digit
 else:
-    print(f"Last digit of {number} is {last_digit} and is 0")
+    last_digit_signed = last_digit
+
+
+if last_digit == 0:
+    print(f"Last digit of {number} is {last_digit_signed} and is 0")
+elif last_digit > 5:
+    print(f"Last digit of {number} is {last_digit_signed} and is greater than 5")
+else:
+    print(f"Last digit of {number} is {last_digit_signed} and is less than 6 and not 0")
