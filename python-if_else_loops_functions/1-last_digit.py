9ebdc56b561be82f8d254fd70428089bb0b51c02
Timi-Holberton <timi.holberton@gmail.com>
2025-05-06 10:42:34 +0200

    1.3 Correction de la comparaison du dernier chiffre signé : utilisation de last_digit_signed pour les comparaisons

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -8,9 +8,9 @@ else:
     last_digit_signed = last_digit
 
 
-if last_digit == 0:
+if last_digit_signed == 0:
     print(f"Last digit of {number} is {last_digit_signed} and is 0")
-elif last_digit > 5:
+elif last_digit_signed > 5:
     print(f"Last digit of {number} is {last_digit_signed} and is greater than 5")
 else:
     print(f"Last digit of {number} is {last_digit_signed} and is less than 6 and not 0")
