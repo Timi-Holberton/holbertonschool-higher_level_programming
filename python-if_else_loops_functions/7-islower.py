8ba32ff3051b7df2d5bc7ba8519c2b1fbeaa829a
Timi-Holberton <timi.holberton@gmail.com>
2025-05-07 10:38:16 +0200

    7.2 bolléen caractère minuscule ou non

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -5,11 +5,11 @@
 
 def islower(c):
     # Vérifie si le caractère c est une lettre minuscule
-    return 97 <= ord(c) <= 122
+    if 97 <= ord(c) <= 122:
+        return True  # Minuscule
+    else:
+        return False  # Pas une minuscule
 
-# Test de la fonction islower avec différents caractères
-print("a is {}".format("lower" if islower("a") else "upper"))
-print("H is {}".format("lower" if islower("H") else "upper"))
-print("A is {}".format("lower" if islower("A") else "upper"))
-print("3 is {}".format("lower" if islower("3") else "upper"))
-print("g is {}".format("lower" if islower("g") else "upper"))
+# même chose mais en une ligne
+# def islower(c):
+# return 97 <= ord(c) <= 122
