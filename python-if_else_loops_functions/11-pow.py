84ae63fc636e32594a11fda469049db004546615
Timi-Holberton <timi.holberton@gmail.com>
2025-05-07 18:48:26 +0200

    11 Puissance avec opérateur **

---
+++ python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+# Définit une fonction appelée pow qui calcule a à la puissance b
+def pow(a, b):
+    # Calcule a élevé à la puissance b avec l'opérateur **
+    puissance = a ** b
+
+    # Renvoie le résultat du calcul au programme qui a appelé la fonction
+    return puissance
