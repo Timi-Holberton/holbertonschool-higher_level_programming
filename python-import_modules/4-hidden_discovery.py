1cb08fdb3846c639d71fcfef8e950dbd0f43195f
Timi-Holberton <timi.holberton@gmail.com>
2025-05-08 22:59:15 +0000

    Extraire et afficher les noms du module hidden_4

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -3,9 +3,7 @@
 import hidden_4
 
 if __name__ == "__main__":
-
-    boite_a_name = dir(hidden_4)
-    boiteSans__ = sorted(name for name in boite_a_name if not name.startswith("__"))
-    for name in boiteSans__:
-        print(name)
-
+        boite_a_name = dir(hidden_4)
+        boiteSans__ = sorted(name for name in boite_a_name if not name.startswith("__"))
+	for name in boiteSans__:
+		print(name)
