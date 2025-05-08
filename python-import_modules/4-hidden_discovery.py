7fe8205f4ddc8db66802eb7e1a5838f88cddad92
Timi-Holberton <timi.holberton@gmail.com>
2025-05-08 22:29:43 +0000

    push

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -4,18 +4,8 @@ import hidden_4
 
 if __name__ == "__main__":
 
-	# On crée une boite (liste) avec tous les noms de hidden_4
-	boite_a_name = dir(hidden_4)
+    boite_a_name = dir(hidden_4)
+    boiteSans__ = sorted(name for name in boite_a_name if not name.startswith("__"))
+    for name in boiteSans__:
+        print(name)
 
-	# On crée une nouvelle boite triée (liste) contenant
-	# seulement les noms sans "__"
-	boiteSans__ = sorted(
-		# Pour chaque name dans la boite à name
-		name for name in boite_a_name
-		# On garde le name s'il ne commence pas par "__"
-		if not name.startswith("__")
-	)
-
-	# On ouvre la boite et on affiche chaque nom dedans
-	for name in boiteSans__:
-		print(name)
