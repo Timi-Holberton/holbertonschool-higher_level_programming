45e410febea87a17d84696785ec22405536e0f9b
Timi-Holberton <timi.holberton@gmail.com>
2025-05-08 16:07:39 +0200

    2.3 ajout d'une condition lorsqu'un seul argument entrée

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,23 +1,24 @@
 #!/usr/bin/python3
 
-# Importation du module sys qui permet d'accéder aux paramètres
-# passés en ligne de commande dans sys.argv
 if __name__ == "__main__":
+    # Importe sys pour accéder aux arguments de la ligne de commande
     import sys
 
-    # Vérifie si aucun argument n'est passé en ligne de commande
-    # sys.argv contient toujours au moins un élément : le nom du script
     if len(sys.argv) == 1:
-        # Si la longueur est 1, cela signifie qu'aucun argument
-        print("0 arguments.")
+        print("0 arguments.")  # Affiche 0 si aucun argument n'est passé
 
-    else:
-        # Si au moins 1 argument, on affiche le nombre total d'arguments
-        # (en soustrayant 1 car on ne compte pas le nom du script lui-même)
-        print("{} arguments:".format(len(sys.argv) - 1))
+    elif len(sys.argv) == 2:
+        # Affiche "1 argument:" si un seul argument est passé
+        # en soustrayant 1 car on ne compte pas le nom du script lui-même
+        print("{} argument:".format(len(sys.argv) - 1))
         for i in range(1, len(sys.argv)):
             # La boucle for commence à 1 car on saute sys.argv[0](nom script)
             # On parcourt tous les arguments donnés par l'utilisateur
             # À chaque itération, i est l'index de l'argument
             # sys.argv[i] est la valeur de l'argument à cet index
+            print("{}: {}".format(i, sys.argv[i]))
+
+    else:
+        print("{} arguments:".format(len(sys.argv) - 1))
+        for i in range(1, len(sys.argv)):
             print("{}: {}".format(i, sys.argv[i]))
