e94cb421ac2cdf524c6205ace5e5027898c022a1
Timi-Holberton <timi.holberton@gmail.com>
2025-05-13 16:02:19 +0200

    Ajout de la fonction add_tuple pour additionner deux tuples et retourner un nouveau tuple

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
 
-def print_matrix_integer(matrix=[[]]):
-    for ligne in matrix:
-        for i in range(len(ligne)):
-            if i != len(ligne) - 1:
-                print("{:d}".format(ligne[i]), end=" ")
-            else:
-                print("{:d}".format(ligne[i]), end="")
-        print()
+def add_tuple(tuple_a=(), tuple_b=()):
+    # On prépare deux listes avec deux éléments chacun : on commence avec [0, 0]
+    a = [0, 0]
+    b = [0, 0]
+
+    # On remplit la liste 'a' avec les éléments de tuple_a s’ils existent
+    for i in range(min(2, len(tuple_a))):
+        a[i] = tuple_a[i]
+
+    # On remplit la liste 'b' avec les éléments de tuple_b s’ils existent
+    for i in range(min(2, len(tuple_b))):
+        b[i] = tuple_b[i]
+
+    # On additionne les deux listes élément par élément et on retourne un tuple
+    return (a[0] + b[0], a[1] + b[1])
