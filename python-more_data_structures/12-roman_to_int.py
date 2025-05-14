9a34c5e675532d2b59d126cbde339c28d78b853d
Timi-Holberton <timi.holberton@gmail.com>
2025-05-14 17:30:09 +0200

    Ajout de la vérification du type d'entrée dans la fonction roman_to_int

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 
 def roman_to_int(roman_string):
+    # Vérifie que l'entrée est bien une chaîne
+    if not isinstance(roman_string, str) or roman_string is None:
+        return 0
+
     # Dictionnaire : chaque chiffre romain a une valeur fixe
     chiffre_romain = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
                'C': 100, 'D': 500, 'M': 1000}
