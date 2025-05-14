bd395de8d03d37b3f0604dd5de0718a9c2db16eb
Timi-Holberton <timi.holberton@gmail.com>
2025-05-14 13:32:28 +0200

    Correction de l'espacement en trop devant ":"

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -5,7 +5,7 @@ def update_dictionary(a_dictionary, key, value):
     # Utilise la méthode .update() pour :
     # - ajouter la paire (key : value) si la clé n'existe pas,
     # - ou modifier la valeur associée à la clé si elle existe déjà
-    a_dictionary.update({key : value})
+    a_dictionary.update({key: value})
 
     # Retourne le dictionnaire mis à jour
     return a_dictionary
