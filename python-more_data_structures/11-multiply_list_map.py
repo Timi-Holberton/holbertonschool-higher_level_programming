991cf2d5c817b51b246ce859337d739fd91377aa
Timi-Holberton <timi.holberton@gmail.com>
2025-05-14 16:26:33 +0200

    Simplification de la fonction multiply_list_map en supprimant une variable intermédiaire

--- python-more_data_structures/11-multiply_list_map.py
+++ python-more_data_structures/11-multiply_list_map.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 def multiply_list_map(my_list=[], number=0):
-    resultat = list(map(lambda x: x * number, my_list))
-    return resultat
+    return list(map(lambda x: x * number, my_list))
+
