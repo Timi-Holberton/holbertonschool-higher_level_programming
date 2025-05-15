0731eba0adfec449a673f7da13ddccc005d3393f
Timi-Holberton <timi.holberton@gmail.com>
2025-05-15 21:26:47 +0200

    Réorganisation des commentaires et des exceptions dans la fonction list_division pour améliorer la lisibilité du code

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -2,18 +2,27 @@
 
 def list_division(my_list_1, my_list_2, list_length):
     resultat = []
+    # list_length est un nombre qui représente la taille max des listes
     for i in range(list_length):
         try:
+            # variable temporaire qui stock le resultat de la division
             division = my_list_1[i] / my_list_2[i]
-        except ZeroDivisionError:
+
+        except ZeroDivisionError: # division par 0
             print("division by 0")
             division = 0
-        except TypeError:
+
+        except TypeError: # ce n'est pas un entier
             print("wrong type")
             division = 0
-        except IndexError:
+
+        except IndexError: # liste plus courte que la limite list_length
             print("out of range")
+            # réinitialise division à 0 pour la prochaine boucle
             division = 0
+
         finally:
+            # ajoute le resultat de division -> resultat même si erreur
             resultat.append(division)
+
     return resultat
