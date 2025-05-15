db2a80969fc5d0c0aabb4f54cdae1e9634d40af7
Timi-Holberton <timi.holberton@gmail.com>
2025-05-15 18:06:14 +0200

    Ajout de la fonction list_division pour effectuer des divisions sécurisées entre deux listes

---
+++ python-exceptions/4-list_division.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+def list_division(my_list_1, my_list_2, list_length):
+    resultat = []
+# list_length est un nombre qui représente la taille max des listes
+    for i in range(list_length):
+        try:
+            value1 = my_list_1[i]
+            value2 = my_list_2[i]
+            # variable temporaire qui stock le resultat de la division
+            division = value1 / value2
+        except IndexError: # liste plus courte que la limite list_lenght
+            print("out of range")
+        # réinitialise division à 0 pour la prochaine boucle
+            division = 0
+        except TypeError:
+            print("wrong type")
+            division = 0
+        except ZeroDivisionError:
+            print("division by 0")
+            division = 0
+        finally: # ajoute le resultat de division -> resultat si pas d'except
+            resultat.append(division)
+    return resultat
