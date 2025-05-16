26839e6d135174b44d3dfd853b6c1f60b3fbe03e
Timi-Holberton <timi.holberton@gmail.com>
2025-05-16 16:51:04 +0200

    Suppression du code redondant dans la fonction print_square pour améliorer la lisibilité

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -16,14 +16,3 @@ def print_square(size):
         raise ValueError("size must be >= 0")
     for _ in range(size):
         print("#" * size)
-
-    if not isinstance(size, int):
-        raise TypeError("size must be an integer")
-    if size < 0:
-        raise ValueError("size must be >= 0")
-    for _ in range(size):
-        print("#" * size)
-
-
-
-
