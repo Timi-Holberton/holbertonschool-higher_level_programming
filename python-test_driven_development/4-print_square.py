9e2df3d95590fe03e15c055e9ac3622b6141fb6c
Timi-Holberton <timi.holberton@gmail.com>
2025-05-16 16:42:43 +0200

    Ajout de la fonction print_square pour afficher un carré de caractères avec gestion des types et des valeurs

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""""
+
+carré de #
+
+"""
+
+
+def print_square(size):
+    """"
+    carré de #
+    """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    for _ in range(size):
+        print("#" * size)
+
+
+
+
+
+
