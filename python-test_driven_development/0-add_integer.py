d511802cba3dcddc23a0f8aeb185dea1d67030ea
Timi-Holberton <timi.holberton@gmail.com>
2025-05-16 11:05:07 +0200

    Ajout de la documentation pour la fonction add_integer

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python3
+"""
 
+    Additionne deux entiers après validation de leurs types.
+
+"""
 def add_integer(a, b=98):
-
+    """
+    Additionne deux entiers après validation de leurs types.
+    """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
     return int(a + b)
+
