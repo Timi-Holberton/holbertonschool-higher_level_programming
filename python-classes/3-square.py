d6be4aa1f93671cdeeb0b23ba63460d7188f4498
Timi-Holberton <timi.holberton@gmail.com>
2025-05-19 11:54:58 +0200

    Ajout de la classe Square avec validation de la taille et méthode pour calculer l'aire

---
+++ python-classes/3-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""""Création d'une class"""
+
+
+class Square:
+    """Ma classe"""
+    pass
+
+    def __init__(self, size=0):
+        self.__size = size
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        return self.__size * self.__size
+
+
