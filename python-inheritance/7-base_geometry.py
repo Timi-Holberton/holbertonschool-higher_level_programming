5a2a31a742743ede8f8f3e69684d91c5e510542b
Timi-Holberton <timi.holberton@gmail.com>
2025-05-26 17:03:23 +0200

    Suppression des attributs inutilisés dans la méthode integer_validator de la classe BaseGeometry

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -13,8 +13,6 @@ class BaseGeometry:
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        self.name = name
-        self.value = value
         if not isinstance(value, int):
             raise TypeError(f"{name} must be an integer")
         if value <= 0:
