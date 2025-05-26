29db61502f875f5e3315cab50b6a6cd3f8e27159
Timi-Holberton <timi.holberton@gmail.com>
2025-05-26 15:54:23 +0200

    Ajout de la classe de base BaseGeometry pour servir de fondation aux classes géométriques.

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Module contenant une classe de base pour la géométrie.
+"""
+
+
+class BaseGeometry:
+    """
+    Classe de base destinée à servir de fondation pour les classes
+    de formes géométriques.
+    """
+    pass
