549fea2a594236509d48123727bd27c682af7e0b
Timi-Holberton <timi.holberton@gmail.com>
2025-05-26 10:51:28 +0200

    Ajout de la fonction lookup pour renvoyer la liste des attributs et méthodes d'un objet

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Fonction qui renvoie la liste des attributs
+et méthodes disponibles d'un objet
+"""
+
+
+def lookup(obj):
+    """Fonction qui renvoie la liste des attributs
+    et méthodes disponibles d'un objet
+    """
+    return (dir(obj))
