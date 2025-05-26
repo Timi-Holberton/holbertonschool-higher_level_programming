f148fc549d12c5905db9fc7074d847439d8b1094
Timi-Holberton <timi.holberton@gmail.com>
2025-05-26 20:16:05 +0200

    Correction des messages d'erreur dans la méthode integer_validator pour une meilleure compatibilité avec les anciennes versions de Python.

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -41,6 +41,6 @@ class BaseGeometry:
             ValueError: Si 'value' est inférieur ou égal à 0.
         """
         if not isinstance(value, int):
-            raise TypeError(f"{name} must be an integer")
+            raise TypeError("{} must be an integer".format(name))
         if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+            raise ValueError("{} must be greater than 0".format(name))
