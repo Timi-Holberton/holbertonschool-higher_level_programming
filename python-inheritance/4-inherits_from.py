10eb16f1b06b8a32167680fc94f9528511e40d62
Timi-Holberton <timi.holberton@gmail.com>
2025-05-26 15:39:13 +0200

    Amélioration de la documentation de la fonction inherits_from pour clarifier les conditions d'héritage et les valeurs de retour.

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -8,19 +8,22 @@ classe donnée.
 def inherits_from(obj, a_class):
     """
     Vérifie si un objet hérite (directement ou indirectement) d'une classe
-    donnée.
+    donnée, sans être une instance directe de cette classe.
 
     Args:
         obj: L'objet à tester.
         a_class: La classe à comparer.
 
     Returns:
-        True si l'objet est une instance d'une classe fille de a_class,
-        False si l'objet est une instance directe de a_class ou sans lien
+        True si l'objet est une instance d'une sous-classe de a_class,
+        False si l'objet est une instance directe de a_class ou sans lien.
     """
-    # issubclass pour tester si la classe de l'objet hérite de a_class
-    # type(obj) donne la classe exacte de l'objet
-    if issubclass(type(obj), a_class):
+    # Si la classe exacte de l'objet est a_class = ce n'est pas un héritage
+    if a_class == type(obj):
+        return False
+    # Si la classe de l'objet = une sous-classe de a_class, c’est un héritage
+    elif issubclass(type(obj), a_class):
         return True
+    # Sinon, ce n’est pas un héritage
     else:
         return False
