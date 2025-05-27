3e2a17341ebf38e5ed96c38c96330531b8b89432
Timi-Holberton <timi.holberton@gmail.com>
2025-05-27 10:53:07 +0200

    Correction de la syntaxe d'importation de BaseGeometry dans la classe Rectangle.

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -14,7 +14,7 @@ rectangle via sa largeur et sa hauteur.
 """
 
 
-BaseGeometry = __import__("7-base_geometry").BaseGeometry
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
