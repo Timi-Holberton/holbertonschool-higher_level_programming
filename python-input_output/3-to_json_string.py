695c6abfa75a2e4c7bc9e9cfbb37dc9eaf5c9dbd
Timi-Holberton <timi.holberton@gmail.com>
2025-06-02 21:56:04 +0200

    Ajout du module to_json_string pour convertir un objet Python en chaîne JSON.

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Module to_json_string
+Ce module fournit une fonction pour convertir un objet Python en chaîne JSON.
+"""
+
+import json
+
+
+def to_json_string(my_obj):
+    """
+    Sérialise un objet Python en une chaîne JSON.
+
+    Paramètre :
+        my_obj : L’objet Python à convertir.
+
+    Retourne :
+        str : La représentation JSON de l’objet.
+    """
+    return my_obj
