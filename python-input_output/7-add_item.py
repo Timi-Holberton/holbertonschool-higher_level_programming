97cb6c1c18c1ef772664694a1243d6df1c0afd58
Timi-Holberton <timi.holberton@gmail.com>
2025-06-06 10:41:55 +0200

    Ajout d'espaces pour améliorer la lisibilité et réorganisation de la gestion des fichiers dans 7-add_item.py

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
 """
 Script principal pour l'ajout d'éléments à une liste JSON stockée
 dans un fichier.
@@ -7,17 +8,20 @@ Ce script lit les arguments passés en ligne de commande
 et les ajoute à une liste persistante contenue dans le fichier 'add_item.json'
 Si ce fichier n'existe pas ou est invalide, une nouvelle liste est initialisée
 """
+
 import sys
+import os
 
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
 filename = "add_item.json"
 args = sys.argv[1:]
-file = load_from_json_file(filename)
-if file:
-    liste = file
+
+if os.path.exists(filename):
+    liste = load_from_json_file(filename)
 else:
     liste = []
+
 liste.extend(args)
 save_to_json_file(liste, filename)
