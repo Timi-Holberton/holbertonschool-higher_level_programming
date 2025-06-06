cd265cb3c752d2d49247f844fb41d3a805828b75
Timi-Holberton <timi.holberton@gmail.com>
2025-06-06 10:34:17 +0200

    Suppression des instructions d'utilisation et des modules externes dans 7-add_item.py pour alléger la documentation.

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -6,19 +6,6 @@ dans un fichier.
 Ce script lit les arguments passés en ligne de commande
 et les ajoute à une liste persistante contenue dans le fichier 'add_item.json'
 Si ce fichier n'existe pas ou est invalide, une nouvelle liste est initialisée
-
-Modules externes requis :
-    - 5-save_to_json_file.py : pour la sérialisation de la liste
-    au format JSON.
-    - 6-load_from_json_file.py : pour la désérialisation de la liste
-    depuis le fichier.
-
-Usage :
-    ./7-add_item.py élément1 élément2 ...
-
-Exemple :
-    $ ./7-add_item.py "apple" "banana"
-    → ajoute "apple" et "banana" à la liste sauvegardée dans add_item.json
 """
 import sys
 
