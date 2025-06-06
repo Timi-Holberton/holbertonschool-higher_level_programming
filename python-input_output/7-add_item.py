940edeeee9fefaa240a20f0f09e928467636f7e0
Timi-Holberton <timi.holberton@gmail.com>
2025-06-06 10:31:41 +0200

    Correction de la gestion des arguments dans 7-add_item.py pour permettre l'ajout direct des éléments à la liste JSON.

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-# -*- coding: utf-8 -*-
 """
 Script principal pour l'ajout d'éléments à une liste JSON stockée
 dans un fichier.
@@ -27,11 +26,11 @@ import sys
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
-args = sys.argv
+args = sys.argv[1:]
 file = load_from_json_file("add_item.json")
 if file:
     liste = file
 else:
     liste = []
-liste.extend(args[1:])
+liste.extend(args)
 save_to_json_file(liste, "add_item.json")
