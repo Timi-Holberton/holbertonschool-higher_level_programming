cddfc383db1daccf4f5264bd33b0885bbd6f93bc
Timi-Holberton <timi.holberton@gmail.com>
2025-06-11 15:47:40 +0200

    Réinitialisation de la structure des utilisateurs en mémoire et ajout de retours JSON pour la création d'utilisateurs.

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -2,20 +2,24 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-users = {"jane": {"username": "jane", "name": "Jane", "age": 28, "city": "Los Angeles"}, "john": {"username": "john", "name": "John", "age": 30, "city": "New York"}}
+users = {}
+
 
 @app.route('/')
 def home():
     return "Welcome to the Flask API!"
+
 
 @app.route('/data')
 def get_user():
     liste_user = list(users.keys())
     return jsonify(liste_user)
 
+
 @app.route('/status')
 def get_statut():
     return "OK"
+
 
 @app.route('/add_user', methods=["POST"])
 def post_add_user():
@@ -26,6 +30,7 @@ def post_add_user():
     users[username] = user_data
     return jsonify({"message": "User added", "user": user_data}), 201
 
+
 @app.route('/users/<username>')
 def get_user_details(username):
     user = users.get(username)
@@ -34,4 +39,6 @@ def get_user_details(username):
     else:
         return jsonify({"error": "User not found"}), 404
 
-if __name__ == "__main__": app.run()
+
+if __name__ == "__main__":
+    app.run()
