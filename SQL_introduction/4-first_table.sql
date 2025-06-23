50c9cd9c6ce01b96286f58bfa354c18363fb585f
Timi-Holberton <timi.holberton@gmail.com>
2025-06-23 14:29:28 +0200

    Ajout d'un script SQL pour créer une table si elle n'existe pas déjà

---
+++ SQL_introduction/4-first_table.sql
@@ -0,0 +1,2 @@
+-- Crée une table avec deux colonnes (id et name) uniquement si elle n'existe pas déjà dans la base de données hbtn_0c_0
+CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
