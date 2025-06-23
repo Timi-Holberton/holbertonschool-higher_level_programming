1fb25a8b10e6e5c7d2c1802b86f505a27c973b06
Timi-Holberton <timi.holberton@gmail.com>
2025-06-23 14:53:42 +0200

    Ajout d'un script SQL pour afficher la structure complète d'une table

---
+++ SQL_introduction/5-full_table.sql
@@ -0,0 +1,2 @@
+-- Affiche la structure complte d'une table, elle retourne deux colonnes : Table (nom de la table) et Create Table (le script CREATE TABLE complet utilisé pour généréer cette table)
+SHOW CREATE TABLE first_table;
