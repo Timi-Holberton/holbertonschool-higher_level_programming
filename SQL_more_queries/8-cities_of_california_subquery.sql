f37ed23581524f2714a43efcbf26e18387f8bad4
Timi-Holberton <timi.holberton@gmail.com>
2025-06-25 18:45:24 +0200

    Ajout d'un commentaire explicatif sur la requête SQL pour sélectionner les villes de Californie.

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -5,3 +5,15 @@ FROM cities, states
 WHERE cities.state_id = states.id
 AND states.name = 'California'
 ORDER BY cities.id ASC;
+
+/*
+La requête sélectionne les colonnes `cities.id` et `cities.name`,
+c’est-à-dire l’identifiant et le nom des villes.
+Les données proviennent des tables `cities` et `states`,
+qui sont liées par la condition `cities.state_id = states.id` pour associer
+chaque ville à son état correspondant. De plus, un filtre est appliqué pour
+ne récupérer que les villes dont l’état est nommé `'California'`.
+Enfin, les résultats sont triés par ordre croissant selon l’identifiant
+des villes (`cities.id`), permettant d’obtenir une liste ordonnée
+des villes californiennes présentes dans la base de données.
+*/
