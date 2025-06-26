fe7af1e08d638574401a1232381d7a495434d673
Timi-Holberton <timi.holberton@gmail.com>
2025-06-26 17:29:23 +0200

    Réorganisation des jointures dans la requête SQL pour afficher les genres associés à la série "Dexter".

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -4,9 +4,9 @@
 -- puis trie les résultats par ordre alphabétique des genres.
 
 SELECT tv_genres.name
-FROM tv_shows
-INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-INNER JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+FROM tv_genres
+INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+INNER JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
 WHERE tv_shows.title = 'Dexter'
 ORDER BY tv_genres.name ASC;
 
