7e68b95d2e83aa16ce558c3d89e310a1ea8a484c
Timi-Holberton <timi.holberton@gmail.com>
2025-06-27 18:13:30 +0200

    Ajout d'un script Python pour lister tous les États depuis la base de données hbtn_0e_0_usa, avec connexion via MySQLdb.

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+Liste tous les États depuis la base hbtn_0e_0_usa.
+Connexion via MySQLdb, sans validation d’arguments.
+Résultats triés par states.id (ordre croissant).
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    # Arguments passés en ligne de commande
+    username = sys.arg[1]
+    password = sys.arg[2]
+    database = sys.arg[3]
+
+    # Connexion à MySQL
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+        )
+
+    # Création d’un curseur
+    cursor = db.cursor()
+
+    # Requête SQL
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+
+    # Affichage des résultats ligne par ligne
+    for row in cursor.fetchall():
+        print(row)
+
+    # Fermeture
+    cursor.close()
+    db.close()
