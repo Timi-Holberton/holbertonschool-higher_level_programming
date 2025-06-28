aab02caa98dcac7098fe8b48f1c4480c3f7c9984
Timi-Holberton <timi.holberton@gmail.com>
2025-06-28 19:20:06 +0200

    Amélioration de la lisibilité de la requête SQL en formatant le code.

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -18,7 +18,8 @@ if __name__ == "__main__":
 
     cursor = db.cursor()
 
-    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC;")
+    cursor.execute(
+        "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC;")
 
     rows = cursor.fetchall()
 
