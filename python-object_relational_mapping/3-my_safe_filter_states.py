4e57a375c5427c2befd45172be7573b9e57f9d9d
Timi-Holberton <timi.holberton@gmail.com>
2025-06-28 21:43:13 +0200

    Ajout du script pour afficher les états correspondant à un nom donné.

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+Script affichant les états dont le nom correspond exactement à un nom donné.
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
+
+    connection = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+        )
+
+    cursor = connection.cursor()
+
+    cursor.execute(
+        "SELECT * FROM states WHERE name = %s ORDER BY id ASC;", (state_name,))
+
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    connection.close()
