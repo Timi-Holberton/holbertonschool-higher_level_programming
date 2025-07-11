4c37d0791c6c2184b4920876831992752dff5f78
Timi-Holberton <timi.holberton@gmail.com>
2025-07-11 17:14:11 +0200

    Ajout fichier JS tâche-6 affiche du texte à partir d'un tableau avec une boucle for

---
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+let tableau = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+let longueur = tableau.length;
+for (let i = 0; i < longueur; i++) {
+  console.log(tableau[i]);
+}
