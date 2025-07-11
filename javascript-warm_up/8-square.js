574c1e98b8c94bcabc0c62c1beb90a46d0758094
Timi-Holberton <timi.holberton@gmail.com>
2025-07-11 22:19:58 +0200

    Semistandard tâche-8 delete ligne

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -16,35 +16,3 @@ if (isNaN(arg)) {
     console.log(row);
   }
 }
-
-
-
-
-
-
-/*Write a script that prints a square
-
--The first argument is the size of the square
--If the first argument can’t be converted to an integer,
- print “Missing size”
--You must use the character X to print the square
--You must use console.log(...) to print all output
--You are not allowed to use var
--You must use a loop (while, for, etc.)
-
-guillaume@ubuntu:~/$ ./8-square.js
-Missing size
-guillaume@ubuntu:~/$ ./8-square.js School
-Missing size
-guillaume@ubuntu:~/$ ./8-square.js 2
-XX
-XX
-guillaume@ubuntu:~/$ ./8-square.js 6
-XXXXXX
-XXXXXX
-XXXXXX
-XXXXXX
-XXXXXX
-XXXXXX
-guillaume@ubuntu:~/$ ./8-square.js -3
-guillaume@ubuntu:~/$ */
