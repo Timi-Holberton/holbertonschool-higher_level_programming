cbbc2838499247b9a46ac388fac64a5c4915c3a0
Timi-Holberton <timi.holberton@gmail.com>
2025-07-14 11:18:35 +0200

    Ajout fichier 1-script, change la couleur lorsqu'on clique sur l'en-tête

---
+++ javascript-dom_manipulation/1-script.js
@@ -0,0 +1,5 @@
+document.getElementById('red_header').addEventListener('click', function () {
+  document.querySelector('header').style.color = '#FF0000';
+});
+// Quand on clique sur le bloc avec l’ID red_header,
+// Cela change la couleur du texte de l’en-tête <header> en rouge.
