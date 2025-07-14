685786aea9f7ff5599fd5ade20c55149b93e1106
Timi-Holberton <timi.holberton@gmail.com>
2025-07-14 15:19:13 +0200

    Ajout fichier 5-script, script qui change le text du header

---
+++ javascript-dom_manipulation/5-script.js
@@ -0,0 +1,6 @@
+document.getElementById('update_header').addEventListener('click', function () {
+  document.querySelector('header').textContent = 'New Header!!!';
+});
+
+// Write a JavaScript script that updates the text of the header element
+// to New Header!!! when the user clicks on the element with id update_header
