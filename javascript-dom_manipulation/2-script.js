8e17a32c7804121f25efd588dff30589573e5257
Timi-Holberton <timi.holberton@gmail.com>
2025-07-14 12:48:16 +0200

    Ajout fichier 2-script, Ajout d’un écoute/clic sur #red_header pour appliquer la classe 'red' au <header>

---
+++ javascript-dom_manipulation/2-script.js
@@ -0,0 +1,7 @@
+document.getElementById('red_header').addEventListener('click', function () {
+  document.querySelector('header').classList.add('red');
+});
+
+// On récupère l'élément avec l'id 'red_header' et on écoute le clic dessus
+// Au clic, on sélectionne la balise <header> et on lui ajoute la classe 'red'
+// déjà défini dans le html (.red)
